5342353afbcb11c00c6088f7ddcc02f2adb04743
georgid <georgi.dzhambazov@upf.edu>
2014-04-24 13:34:21 +0200

    minor changes

--- AlignmentStep/doit.py
+++ AlignmentStep/doit.py
@@ -15,14 +15,15 @@ from RecordingSegmenter import RecordingSegmenter
 
 PATH_TO_HTK_MODEL = '/Volumes/IZOTOPE/adaptation_data_NOT_CLEAN/syllablingDB/hmmdefs.gmmlrmean_map_2'
 
-PATH_TO_HTK_MODEL ='/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
 PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/hts_female_hmmdefs.gmmlrmean_map_2'
 
+PATH_TO_HTK_MODEL ='/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
 
 
 
 
-PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/multipleGauss/hmm4/HTS_japan_female.gmmlrmean_map_2'
+
+# PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/multipleGauss/hmm4/HTS_japan_female.gmmlrmean_map_2'
 
 # PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata/model_output/hmmdefs'
 
@@ -34,7 +35,8 @@ PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapte
 # PATH_TO_NOTCLEAN_ADAPTDATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_NOT_CLEAN/04_Hamiyet_Yuceses_-_Bakmiyor_Cesm-i_Siyah_Feryade/'      
 PATH_TO_NOTCLEAN_ADAPTDATA = '/tmp/audio/'
 
-
+PATH_TEST_DATASET = '/Users/joro/Documents/Phd/UPF/sertan_sarki_5songs_peaksSynthesis/'    
+PATH_TEST_DATASET = '/Volumes/IZOTOPE/sertan_sarki/'    
 
           
 
@@ -51,7 +53,6 @@ def doitForTestPiece(compositionName, recordingDir):
    
     ####### prepare composition! ############
         
-        PATH_TEST_DATASET = '/Volumes/IZOTOPE/sertan_sarki/'    
         pathToComposition = os.path.join(PATH_TEST_DATASET, compositionName)
         os.chdir(pathToComposition)
         pathToSymbTrTxt = os.path.join(pathToComposition, glob.glob("*.txt")[0])
@@ -81,20 +82,22 @@ if __name__ == '__main__':
     ############################# doit for recording ###############################        
 #         
 #         
-        compositionName = 'nihavent--sarki--aksak--koklasam_saclarini--artaki_candan'
-        recordingDir = '20_Koklasam_Saclarini'
-         
-#         compositionName = 'segah--sarki--curcuna--olmaz_ilac--haci_arif_bey'
-#         recordingDir = '21_Recep_Birgit_-_Olmaz_Ilac_Sine-i_Sad_Pareme'
-         
+       
+        compositionName = 'segah--sarki--curcuna--olmaz_ilac--haci_arif_bey'
+        recordingDir = '21_Recep_Birgit_-_Olmaz_Ilac_Sine-i_Sad_Pareme'
+           
 #         compositionName = 'nihavent--sarki--turkaksagi--nerelerde_kaldin--ismail_hakki_efendi'
 #         recordingDir = '3-12_Nerelerde_Kaldin'
 #         
-#         compositionName = 'nihavent--sarki--curcuna--kimseye_etmem--kemani_sarkis_efendi'
-#         recordingDir = '03_Bekir_Unluataer_-_Kimseye_Etmem_Sikayet_Aglarim_Ben_Halime'
+        compositionName = 'nihavent--sarki--curcuna--kimseye_etmem--kemani_sarkis_efendi'
+        recordingDir = '03_Bekir_Unluataer_-_Kimseye_Etmem_Sikayet_Aglarim_Ben_Halime'
 
 #         compositionName = 'muhayyerkurdi--sarki--duyek--ruzgar_soyluyor--sekip_ayhan_ozisik'
 #         recordingDir = '1-05_Ruzgar_Soyluyor_Simdi_O_Yerlerde'
+
+        compositionName = 'nihavent--sarki--aksak--koklasam_saclarini--artaki_candan'
+        recordingDir = '20_Koklasam_Saclarini'
+         
 # #                  
         doitForTestPiece(compositionName, recordingDir)
 
--- AlignmentStep/experiments/doitAll.py
+++ AlignmentStep/experiments/doitAll.py
@@ -6,7 +6,16 @@ Main for
 from Aligner import PHRASE_ANNOTATION_EXT, openAlignmentInPraat
 from RecordingSegmenter import RecordingSegmenter
 from evaluation.WordLevelEvaluator import evalPhraseLevelError
+from Adapt import PATH_TO_OUTPUT, MODEL_NAME, PATH_TO_CLEAN_ADAPTDATA, adapt,\
+    MLLR_EXT, MAP_EXT, NUM_MAP_ITERS
+from matplotlib.sphinxext.plot_directive import out_of_date
+import os
+import sonicVisTextPhnDir2mlf
+from doit import PATH_TEST_DATASET
 
+ # modelURI from adaptation script
+MODEL_URI = os.path.join(PATH_TO_OUTPUT, MODEL_NAME + MLLR_EXT + MAP_EXT + str(NUM_MAP_ITERS-1) )
+        
 
 PATH_TO_HTK_MODEL = '/Volumes/IZOTOPE/adaptation_data_NOT_CLEAN/syllablingDB/hmmdefs.gmmlrmean_map_2'
 
@@ -14,18 +23,19 @@ PATH_TO_HTK_MODEL = '/Volumes/IZOTOPE/adaptation_data_NOT_CLEAN/syllablingDB/hmm
 
 
 ########## FEMALE
-PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/hts_female_hmmdefs.gmmlrmean_map_2'
+# PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/hts_female_hmmdefs.gmmlrmean_map_2'
 # PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/multipleGauss/hmm4/HTS_japan_female.gmmlrmean_map_2'
 
 
 ######## MALE
 
-PATH_TO_HTK_MODEL ='/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
+# PATH_TO_HTK_MODEL ='/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
 
 # PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/syllablingDB.gmmlrmean_map_2'
 
 #PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/hmmdefs.gmllrmean_gmmlr_4' 
 
+# NOT_CLEAN : deprecated! 
 #PATH_TO_NOTCLEAN_ADAPTDATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_NOT_CLEAN/syllablingDB/'
 # PATH_TO_NOTCLEAN_ADAPTDATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_NOT_CLEAN/04_Hamiyet_Yuceses_-_Bakmiyor_Cesm-i_Siyah_Feryade/'      
 
@@ -34,19 +44,19 @@ PATH_TO_HTK_MODEL ='/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted
 
 
 ''' lazy mathod on top of doit
+NOTE: first run AdaptationStep/Adapt.adapt(). THen URI to model  is constructed directly from there by the global variable. MODEL_NAME
 '''
         
 def doitForAdaptationFile(pathTodata,  audioName):
-        
-    
+        ################### ADAPTATION: ################
         
         pathToAudio =    pathTodata + audioName + '.wav'
         
         
         phraseAnnoURI = pathTodata +  audioName + PHRASE_ANNOTATION_EXT
         
-        
-        outputHTKPhoneAlignedURI = RecordingSegmenter.alignOneChunk(PATH_TO_HTK_MODEL, '/tmp/audioTur', "", pathToAudio, 1)
+      
+        outputHTKPhoneAlignedURI = RecordingSegmenter.alignOneChunk(MODEL_URI, '/tmp/audioTur', "", pathToAudio, 1)
 
         diff = evalPhraseLevelError(phraseAnnoURI, outputHTKPhoneAlignedURI)
         
@@ -64,42 +74,43 @@ if __name__ == '__main__':
 #         audioName = 'amaury_3_slow1'
 # 
  
-    PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
-    PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/multipleGauss/hmm4/HTS_japan_male.gmmlrmean_map_2'
+#     PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/multipleGauss/hmm4/HTS_japan_male.gmmlrmean_map_2'
 
   
-    PATH_TO_CLEAN_ADAPTDATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_soloVoice/04_Hamiyet_Yuceses_-_Bakmiyor_Cesm-i_Siyah_Feryade/'    
-    PATH_TO_CLEAN_ADAPTDATA  = '/Users/joro/Documents/Phd/UPF/adaptation_data_soloVoice/syllablingDB/'
-    PATH_TO_CLEAN_ADAPTDATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_soloVoice/HTS_japan_male/'
-      
+
+#       
+#     # female  
 #     audioName = 'nitech_jp_song070_f001_070'
-    audioName = 'nitech_jp_song060_m001_003'
-      
-    error = doitForAdaptationFile(PATH_TO_CLEAN_ADAPTDATA, audioName)
-     
-    print error
-#      
-    
+# 
+#     # male
+# #     audioName = 'nitech_jp_song060_m001_003'
+#        
+#     error = doitForAdaptationFile(PATH_TO_CLEAN_ADAPTDATA + MODEL_NAME + '/' , audioName)
+#       
+#     print error
+# #      
+#     
     ####################### test with recorded clean voice ######################################
     
-#     PATH_TO_HTK_MODEL = '/Users/joro/Documents/Phd/UPF/METUdata//model_output/adapted/HTS_japan_male.gmmlrmean_map_2'
-# 
-#     
 #     PATH_TEST_DATASET = '/Volumes/IZOTOPE/sertan_sarki/' # this is the clean dataset 
-#      
+#       
 #         # these two lines are only for case of GEORGI-reocroded voice
 #     compositionName = 'nihavent--sarki--aksak--koklasam_saclarini--artaki_candan/'
 #     PATH_TO_CLEAN_ADAPTDATA = PATH_TEST_DATASET + compositionName + 'GEORGI/'; 
-#      
+#       
 #     audioName = 'GEORGI_20_Koklasam_Saclarini_zemin_from_19.292461_to_32.514873'
-# # #          audioName = '04_Hamiyet_Yuceses_-_Bakmiyor_Cesm-i_Siyah_Feryade_gazel_1'
-# #       
-#  
-#            
-#     error = doitForAdaptationFile(PATH_TO_CLEAN_ADAPTDATA, audioName)
-#     audioName = 'GEORGI_20_Koklasam_Saclarini_zemin_from_32.514873_to_46.047599'
-#     
-#     error2 = doitForAdaptationFile(PATH_TO_CLEAN_ADAPTDATA, audioName)
-#           
-#     error = (error + error2) / 2
-#     print error
+# # # #          audioName = '04_Hamiyet_Yuceses_-_Bakmiyor_Cesm-i_Siyah_Feryade_gazel_1'
+
+
+
+################################### test with one chunk of sarki recording ####################################
+    compositionName = 'nihavent--sarki--aksak--koklasam_saclarini--artaki_candan/' 
+    
+    PATH_TO_DATA = '/Users/joro/Documents/Phd/UPF/adaptation_data_soloVoice/20_Koklasam_Saclarini_Synth/' ;
+   
+    audioName ='20_Koklasam_Saclarini_nakarat_from_46_047599_to_59_716561'
+       
+          
+    error = doitForAdaptationFile(PATH_TO_DATA, audioName)
+    
+    print error
